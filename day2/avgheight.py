d0f422902eef4217cec94a96726f1ce40ee138c6
SHEIK-32 <mohammedmohideen555@gmail.com>
2022-12-10 11:57:34 +0530

    average height program

---
+++ day2/avgheight.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import random
+l=[10,125,145,135,162,123,147,169,185,163,148,154,126,147,158,159,168]  
+sum=sum(l)
+b=len(l)
+avg=sum/b
+print(f"\n{avg} is the average height.")
