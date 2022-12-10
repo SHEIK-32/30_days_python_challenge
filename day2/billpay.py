c4a394ad6affed914276b26796d5c58f89ca9567
SHEIK-32 <mohammedmohideen555@gmail.com>
2022-12-10 11:51:45 +0530

    Completed random number

---
+++ day2/billpay.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import random 
+l=["Sheik","Mahe","Bharath","Raj","Mohideen","Mohammed","Vijay","Ajith"]
+a=random.choice(l)  
+print(f"\n{a} has to pay the bill.")"{a} has to pay the bill.")
